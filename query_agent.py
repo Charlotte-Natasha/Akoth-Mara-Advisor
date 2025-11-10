10a2b837a3f848bde935915d8fddc4e974fd8d28
Charlotte <natashaonyango97@gmail.com>
2025-11-10 16:21:05 +0100

    styled abit

--- query_agent.py
+++ query_agent.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 # Load environment variables
 load_dotenv()
 
-from langchain_community.vectorstores import Chroma
+from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate
